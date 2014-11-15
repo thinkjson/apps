f8498bb2f694dd52b59a27d58c5f2d2f35bf0d75
Mark Cahill <mark@thinkjson.com>
2014-11-15 14:22:14 -0500

    Added remove buttons, focus last element on add

--- decide/js/main.js
+++ decide/js/main.js
@@ -2,12 +2,19 @@
 
 var app = angular.module('decide', []);
 
-app.controller('MainController', ['$scope', function($scope) {
+app.controller('MainController', ['$scope', '$timeout', function($scope, $timeout) {
 	$scope.choices = localStorage.choices ? JSON.parse(localStorage.choices) : ['stay', 'go'];
 	$scope.choice = null;
 
 	$scope.add = function() {
 		$scope.choices.push('');
+		$timeout(function() {
+			$('input[type="text"]:last').focus();
+		}, 0);
+	}
+
+	$scope.remove = function(i) {
+		$scope.choices.splice(i, 1);
 	}
 
 	$scope.decide = function() {
