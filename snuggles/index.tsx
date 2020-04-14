ce3928e57a92e55fd160e490310015dae2a3c273
Unknown <unknown@example.com>
2020-04-13 21:28:40 -0400

    Switched to smaller libraries for faster load time

--- snuggles/index.tsx
+++ snuggles/index.tsx
@@ -1,12 +1,14 @@
 
-import * as React from "react";
-import { render } from "react-dom";
+import { Component, render, createElement } from "preact";
+import "./index.css";
+
+const h = createElement; // preact weirdness
 
 interface AppState {
     turn: number;
 }
 
-class App extends React.Component<{}, AppState> {
+class App extends Component<{}, AppState> {
     children = ["Caleb", "Malachi", "Ellie"];
 
     constructor(props: any) {
@@ -22,15 +24,17 @@ class App extends React.Component<{}, AppState> {
         const tonight = [...this.children, ...this.children].slice(this.state.turn, this.state.turn + 3);
 
         return (
-            <div className="container mt-5">
+            <div className="container">
                 <p>Whose turn is it to do the dishwasher?</p>
-                <button onClick={() => this.select(0)} className={"btn btn-lg " + (this.state.turn === 0 ? "btn-primary" : "btn-secondary")}>Caleb</button>
-                <button onClick={() => this.select(1)} className={"btn btn-lg " + (this.state.turn === 1 ? "btn-primary" : "btn-secondary")}>Malachi</button>
-                <button onClick={() => this.select(2)} className={"btn btn-lg " + (this.state.turn === 2 ? "btn-primary" : "btn-secondary")}>Ellie</button>
+                <button onClick={() => this.select(0)} className={(this.state.turn === 0 ? "button-primary" : "")}>Caleb</button>
+                <button onClick={() => this.select(1)} className={(this.state.turn === 1 ? "button-primary" : "")}>Malachi</button>
+                <button onClick={() => this.select(2)} className={(this.state.turn === 2 ? "button-primary" : "")}>Ellie</button>
 
-                <div className="mt-4">{tonight[0]} is on my right</div>
-                <div>{tonight[1]} is on my left</div>
-                <div>{tonight[2]} is on the floor</div>
+                <div className="results">
+                    <div>{tonight[0]} is on my right</div>
+                    <div>{tonight[1]} is on my left</div>
+                    <div>{tonight[2]} is on the floor</div>
+                </div>
             </div>
         );
     }
